3caddfd357fdf2b57626499a8e6c9ffc625898ea
Strovmirviaska <104303603+Strv-BOT@users.noreply.github.com>
2022-05-27 19:37:39 +0700

    Update meta.py

--- meta.py
+++ meta.py
@@ -1,7 +1,7 @@
 import os 
 os.system('git pull')
 os.system('termux-setup-storage')
-print("\33[37mSelamat Menggunakan Tools Saya !!!")
+print("Selamat Menggunakan Tools Saya !!!")
 import run
 Main()
 
