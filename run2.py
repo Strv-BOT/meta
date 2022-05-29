6db4cdcc1f1262337a26f12cdda920a877d48b39
Strovmirviaska <104303603+Strv-BOT@users.noreply.github.com>
2022-05-30 06:12:09 +0800

    Update run2.py

--- run2.py
+++ run2.py
@@ -134,32 +134,32 @@ class Main:
 		 if hoga in ["1", "01"]:
 			
 			    self.fbtua()
-			else: 
-				notice()
-				exit()
+			
+				
+				
 		elif hoga in ["2", "02"]:
 			
 			    self.fbtua()
-			else: 
-				notice()
-				exit()
+			
+				
+				
 		elif hoga in ["3", "03"]:
 			
 				self.old4_5()
-			else: 
-				notice()
-				exit()
+			
+				
+				
 		elif hoga in ["4", "04"]:
 			
 				self.email()
-			else: 
-				notice()
-				exit()
+			
+				
+				
 		
 			
 			
 		
-			Main()
+			Main(
 
 	def oldcrack(self):
 		x = 11111111
