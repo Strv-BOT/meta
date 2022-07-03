fbbccacaa897364956648b127d6b9000b2ce0f31
Strovmirviaska <104303603+Strv-BOT@users.noreply.github.com>
2022-07-03 13:42:52 +0800

    Update run2.py

--- run2.py
+++ run2.py
@@ -176,9 +176,9 @@ def logo():
 \33[37m ██████╔╝    ██║    ██║  ██║   ╚██╔╝
 \33[37m ╚═════╝     ╚═╝    ╚═╝  ╚═╝    ╚═╝
 \33[31mGITHUB   : \33[1;31mPrivate  
-\x1b[1;92[FACEBOOK : \33[1;96mTeddy Cahyo Putra Pangembara           
-\x1b[0;33[WHATSAPP : \33[1;33m082290238779
-\x1b[0;33[SUPPORT  : \33[1;33mDevice 64 Bit     
+\33[1;96mFACEBOOK : \33[1;96mTeddy Cahyo Putra Pangembara           
+\33[1;96mWHATSAPP : \33[1;33m082290238779
+\x1b[0;33[SUPPORT  : \33[1;96mDevice 64 Bit     
 \x1b[0;33[SCRIP    : \33[1;96mHacking & Cracking 
 \x1b[0;33[VERSION  : \33[1;96mV.02 Beta.Test  """)
 
